687ab79ca34a38a3adf795bca4aca83119202da5
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2025-11-14 02:20:14 +0400

    add flutter_dotenv

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'welcome.dart';
 
-void main() async {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
